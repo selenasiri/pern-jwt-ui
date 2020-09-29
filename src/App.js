740ba85b1b43b8fc5a8299d3463d6fa1108f64c9
Selena Siri <selenasiri@gmail.com>
2020-09-29 15:11:50 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import './App.css'
+
+//components
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <div className='container'>
+          <Switch>
+            <Route
+              exact
+              path='/login'
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to='/dasboard' />
+                )
+              }
+            />
+            <Route
+              exact
+              path='/register'
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to='/login' />
+                )
+              }
+            />
+            <Route
+              exact
+              path='/dashboard'
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to='/login' />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
